0b7a4b8a7380a46dfcab6d5b571caca8f12da92c
Agustin Pane <agustin.pane@gmail.com>
2018-05-16 17:47:58 +0200

    IOT LAB 2 FIRST COMMIT

---
+++ uartecho_CC1350_LAUNCHXL_tirtos_ccs/Debug/uartecho.d
@@ -0,0 +1,71 @@
+# FIXED
+
+uartecho.obj: ../uartecho.c
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stddef.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/drivers/GPIO.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/drivers/UART.h
+uartecho.obj: ../Board.h
+uartecho.obj: ../CC1350_LAUNCHXL.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/drivers/PIN.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdbool.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/ioc.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/interrupt.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/debug.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/cpu.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/gpio.h
+uartecho.obj: C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdio.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdarg.h
+uartecho.obj: E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/string.h
+
+../uartecho.c: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/drivers/GPIO.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/drivers/UART.h: 
+../Board.h: 
+../CC1350_LAUNCHXL.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/drivers/PIN.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdbool.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/interrupt.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/debug.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/cpu.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_2_10_00_36/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdio.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdarg.h: 
+E:/Utilidades/CodeComposer/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/string.h: 
